106538858a38471f7d04937e2efdbc4ffe36cfc2
Ahtisham <Ahtisham0747@gmail.com>
2025-04-07 20:13:30 +0530

    adding 2 files

---
+++ Calc.js
@@ -0,0 +1,13 @@
+let a = 10 ;
+let b = 20 ;
+let x = a + b ;
+let y = a - b ;
+let z = a * b ;
+let n = a / b ;
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(n);
+
---
+++ OddIf.js
@@ -0,0 +1,9 @@
+let n = 8 ;
+let remainder = n % 2;
+if(remainder == 0){
+    console.log(n + " Is a Even Number");
+    
+}else{
+    console.log(n + " Is a Odd Number");
+
+}
